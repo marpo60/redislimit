e1e8a7757f9894caeaf32165c9b04793af1c668c
Marcelo Dominguez <marcelo@wyeworks.com>
2014-11-01 19:16:39 -0200

    Add redis

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 ruby "2.1.2"
 
 gem 'sinatra', '1.1.0'
+gem 'redis'
--- web.rb
+++ web.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'redis'
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 
 get '/' do
-  "Hello, world"
+  REDIS.hgetall("forbidden").to_s
+end
+
+get '/*' do
+  path = params[:splat].first
+  REDIS.hincrby "forbidden", path, 1
 end
